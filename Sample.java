8f0eb473d508382cd3c47c031c63f0480b8935b2
Himani.Vaidya <85447801+hvaidya14@users.noreply.github.com>
2025-02-27 06:26:39 -0800

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -6,3 +6,28 @@
 
 
 // Your code here along with comments explaining your approach
+class Logger {
+    Map<String, Integer> m;
+    public Logger() {
+        m = new HashMap<>();
+    }
+    public boolean shouldPrintMessage(int timestamp, String message) {
+        if (m.containsKey(message)) {
+            int printtime = m.get(message)+10;
+            if(timestamp < printtime) {
+                return false;
+            }
+            m.put(message, timestamp);
+            return true;
+        } else {
+            m.put(message, timestamp);
+            return true;
+        }
+        
+    }
+}
+/**
+ * Your Logger object will be instantiated and called as such:
+ * Logger obj = new Logger();
+ * boolean param_1 = obj.shouldPrintMessage(timestamp,message);
+ */
